fa7379bfd30df4d82d22d587590bc44389cf91bc
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-02-10 01:17:26 +0600

    play with DOM and Event

---
+++ script.js
@@ -0,0 +1,20 @@
+// checking js external file added or not by alert 
+//alert('Hello')
+
+// accessing elements by class name
+/* const allButton = document.getElementsByClassName('button-test');
+//console.log(allButton)
+// accessing each elements or class by using for loop
+for(const button of allButton){
+    button.addEventListener('click', function(event){
+        //console.log(event.target.parentNode)
+        if(event.target.id == "prime"){
+            document.getElementById('prime').style.color = 'cyan'
+        } else{
+            document.getElementById('prime').style.color = 'red'
+        }
+    })
+}
+// accessing elements by their id
+const prime = document.getElementById('prime');
+//console.log(prime) */
