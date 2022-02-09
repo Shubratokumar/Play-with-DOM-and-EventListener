a5d0dfd67bb2c9b5332c357a30fc6597a665b120
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-02-10 03:08:46 +0600

    input and table added

---
+++ project/project.js
@@ -0,0 +1,16 @@
+//cheking external js file works or not
+//alert("Project")
+
+// project work starts here
+document.getElementById('add-btn').addEventListener('click', function(){
+    const inputValue = document.getElementById('input-value').value;
+    //console.log(inputValue)
+
+    // Validation
+    if(inputValue  ==  ""){
+        alert("Please enter a valid value.")
+    } 
+    else{
+
+    }
+})
